5d1727534556c4b3f0d03fd17387adf3a6c79125
nyxxyr <nyxxyr@gmail.com>
2020-09-03 13:58:43 +0700

    adding tugas 9

--- src/App.js
+++ src/App.js
@@ -4,21 +4,29 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form className="form-control">
+        <header>
+          <h1>Form Pembelian Buah</h1>
+        </header>
+        <div className="custom-flex">
+          <div className="form-input">
+            <label className="input-label">Nama Pelanggan</label>
+            <input style={{marginLeft: '2rem'}} type="text" />
+          </div>
+          <div className="form-input choice">
+            <label className="input-label">Daftar Item</label>
+            <ul style={{marginLeft: '4rem'}} className="items">
+              <li><input type="radio" name="pilihan" /> Semangka</li>
+              <li><input type="radio" name="pilihan" /> Jeruk</li>
+              <li><input type="radio" name="pilihan" />Nanas</li>
+              <li><input type="radio" name="pilihan" />Salak</li>
+              <li><input type="radio" name="pilihan" />Anggur</li>
+            </ul>
+          </div>
+          <input className="btn" type="submit" value="Kirim" />
+        </div>
+      </form>
     </div>
   );
 }
