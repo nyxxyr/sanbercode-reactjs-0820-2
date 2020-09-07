718db2ec53d4082a213c7f1c217ea6f3639e74af
nyxxyr <nyxxyr@gmail.com>
2020-09-07 14:12:54 +0700

    adding component for tugas11

---
+++ src/Tugas11/Timer.js
@@ -0,0 +1,50 @@
+import React from 'react'
+
+class Timer extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            isShow: true,
+            time: 10,
+            clock: 0
+        }
+    }
+    componentDidMount(){
+        this.timerCount = setInterval(
+            () => this.countDown()
+        , 1000)
+    }
+    componentDidUpdate(){
+        if(this.state.isShow === true){
+            if(this.state.time === 0){
+                this.setState({
+                    isShow: false
+                })
+                this.componentWillUnmount()
+            }
+        }
+    }
+    componentWillUnmount(){
+        clearInterval(this.timerCount)
+    }
+    countDown(){
+        this.date = new Date()
+        this.setState({
+            time: this.state.time - 1,
+            clock: `${this.date.getHours()}:${this.date.getMinutes()}:${this.date.getSeconds()}`
+        })
+    }
+    render(){
+        let condition = this.state.isShow ?
+                <div>
+                    <h2>Hai {this.state.clock}</h2>
+                    <h2>Hai {this.state.time}</h2> 
+                </div>
+                : ''
+        return(
+            <div>{condition}</div>
+        )
+    }
+}
+
+export default Timer
