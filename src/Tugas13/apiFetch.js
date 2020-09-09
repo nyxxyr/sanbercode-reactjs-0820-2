a366ebd06556c742bccec05cf2ce87237151d658
nyxxyr <nyxxyr@gmail.com>
2020-09-09 23:26:52 +0700

    updating mistaken input after delete

--- src/Tugas13/apiFetch.js
+++ src/Tugas13/apiFetch.js
@@ -66,7 +66,7 @@ const ApiFetch = () => {
                     name: '',
                     weight: '',
                     price: '',
-                    index: 0
+                    id: 0
                 })
             })
 
@@ -83,7 +83,7 @@ const ApiFetch = () => {
                         name: '',
                         weight: '',
                         price: '',
-                        index: 0
+                        id: 0
                     })
                 })
         }else{
@@ -94,7 +94,7 @@ const ApiFetch = () => {
                         name: '',
                         weight: '',
                         price: '',
-                        index: 0
+                        id: 0
                     })
                 })
         }
